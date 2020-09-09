3371379f3e0359205a8022b4bfa40ae4f1936090
Gabriel Guimaraes Carvalho <ggc5@motorola.com>
2020-09-09 08:30:46 -0300

    v1

--- proc_image.py
+++ proc_image.py
@@ -3,11 +3,16 @@ from argparse import ArgumentParser
 
 def arg_parser():
     parser = ArgumentParser()
-    parser.add_argument('path', type = str, help  = 'path to image')
-    parser.add_argument('name', type = str, help  = 'name: grayscale, halftone, mean, gaussian,\
-                                                           sharpen, laplacian, emboss,motion')
-    parser.add_argument('save', type = str2bool, help = 'Boolean to save output. See README.md for\
-                                                         list o allowed values.')
+    parser.add_argument("path", type = str, help  = "Path to image. Example: ~/Desktop/cats.png")
+
+    parser.add_argument("name", type = str, help  = "processing name: grayscale, halftone, mean, gaussian,\
+                                                                      sharpen, laplacian, emboss, motion,\
+                                                                      x_edge, y_edge, brighten, darken,\
+                                                                      identity, rot90, rot180, rotm90,\
+                                                                      vert_flip, hor_flip, downscale, negative")
+
+    parser.add_argument("save", type = str2bool, help = "Boolean to save output. See README.md\
+                                                         for list of allowed values.")
     return parser
 
 def main():
@@ -20,5 +25,5 @@ def main():
 
     proc_image(path, name, save)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
